63d25ce69cc8a5b5bfc29c8dd13240b6916d8f12
Edier55 <edierguerra55@gmail.com>
2025-01-23 16:36:23 -0500

    feat(backend): crea una schema para el usuario, los permisos cors y una ruta para corraborar los datos

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# Lista de orígenes permitidos (React suele correr en localhost:3000 durante el desarrollo)
+origins = [
+    "http://localhost:5173",  # Origen del frontend en desarrollo
+]
+
+# Agregar el middleware de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Lista de orígenes permitidos
+    allow_credentials=True,  # Permitir cookies y credenciales
+    allow_methods=["*"],  # Permitir todos los métodos HTTP (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permitir todos los headers
+)
+
+class UserSchema(BaseModel):
+    id:int
+    name:str
+    lastName:str
 
 @app.get("/")
 def root():
     return "Hola, FastAPI"
+
+@app.post("/users/")
+def message_user(users_data:UserSchema):
+    print(users_data)
+    return {"message":"Son estos los datos","datos":users_data}

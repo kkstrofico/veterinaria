b597a53c7a7557a457d6d3c023a81e482b6612df
Edier55 <edierguerra55@gmail.com>
2025-01-29 20:13:03 -0500

    feat(api): agrega los endpoints del veterinario

---
+++ backend/api/veterinario.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from database import get_db
+from schemas import VeterinarioLogin,VeterinarioSchema
+from models import Veterinario
+
+router = APIRouter(prefix="/api/v1/veterinario",tags=["veterinario"])
+
+@router.post("/",response_model=VeterinarioSchema)
+def login(veterinario_data:VeterinarioLogin,db:Session = Depends(get_db)):
+    veterinario = db.query(Veterinario).filter(Veterinario.username == veterinario_data.username, Veterinario.password == veterinario_data.password).first()
+    if not veterinario: 
+        {"message":"Usuario no encontrado o contraseña invalida"} 
+    return veterinario

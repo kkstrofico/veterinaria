d21bb08dd8f7769e2bf9e3107e33c8734be155f7
Edier55 <edierguerra55@gmail.com>
2025-01-30 10:51:03 -0500

    refactor(api): hace que el endpoint del login retorne un diccionario con el token

--- backend/api/veterinario.py
+++ backend/api/veterinario.py
@@ -3,12 +3,17 @@ from sqlalchemy.orm import Session
 from database import get_db
 from schemas import VeterinarioLogin,VeterinarioSchema
 from models import Veterinario
+from auth import encode_token
 
 router = APIRouter(prefix="/api/v1/veterinario",tags=["veterinario"])
 
-@router.post("/",response_model=VeterinarioSchema)
+@router.post("/")
 def login(veterinario_data:VeterinarioLogin,db:Session = Depends(get_db)):
     veterinario = db.query(Veterinario).filter(Veterinario.username == veterinario_data.username, Veterinario.password == veterinario_data.password).first()
     if not veterinario: 
-        {"message":"Usuario no encontrado o contraseña invalida"} 
-    return veterinario
+        return {"message":"Usuario no encontrado o contraseña invalida"} 
+    token = encode_token({"sub":veterinario.username,"id":veterinario.id})
+    return {
+        "access_token":token,
+        "token_type":"bearer"
+        }
